7f3c5ef3f189e70d704dfa783925b1b64e4ab5b9
cooperchien <cooperchien@gmail.com>
2023-10-28 17:35:26 -0700

    Update Branding to say CooperChien AI

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,7 +51,7 @@ export const Badge = (props: Props) => {
       "text-align": 'center',
       color: props.poweredByTextColor ?? defaultTextColor,
       "background-color": props.badgeBackgroundColor ?? '#ffffff'
-    }}>Powered by Cooper
+    }}>Powered by Cooper Chien AI 
    
         
   
